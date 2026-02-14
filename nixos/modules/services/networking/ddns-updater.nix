4f756c578000990cba649d0c02dbd93a0c265d38
Lord-Valen <lord_valen@protonmail.com>
2026-02-14 13:41:10 -0500

    nixos/ddns-updater: add rfc042 settings

--- nixos/modules/services/networking/ddns-updater.nix
+++ nixos/modules/services/networking/ddns-updater.nix
@@ -6,6 +6,9 @@
 }:
 let
   cfg = config.services.ddns-updater;
+
+  settingsFormat = pkgs.formats.json { };
+  configFile = settingsFormat.generate "config.json" cfg.settings;
 in
 {
   options.services.ddns-updater = {
@@ -18,10 +21,14 @@ in
       description = "Environment variables to be set for the ddns-updater service. DATADIR is ignored to enable using systemd DynamicUser. For full list see <https://github.com/qdm12/ddns-updater>";
       default = { };
     };
+
+    settings = lib.mkOption {
+      type = settingsFormat.type;
+      description = "Configuration for ddns-updater, see <https://github.com/qdm12/ddns-updater#configuration> for supported values.";
+    };
   };
 
   config = lib.mkIf cfg.enable {
-
     systemd.services.ddns-updater = {
       description = "DDNS-updater service";
       wantedBy = [ "multi-user.target" ];
@@ -30,6 +37,9 @@ in
       environment = cfg.environment // {
         DATADIR = "%S/ddns-updater";
       };
+      preStart = lib.mkIf (cfg.settings != { }) ''
+        ln -sf ${configFile} "$DATADIR/config.json"
+      '';
       serviceConfig = {
         TimeoutSec = "5min";
         ExecStart = lib.getExe cfg.package;
