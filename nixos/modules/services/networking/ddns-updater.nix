8a0e357e59ebf209e25d875674d36276eb1302c9
Lord-Valen <lord_valen@protonmail.com>
2026-02-14 14:14:05 -0500

    nixos/ddns-updater: make environment a freeform module

--- nixos/modules/services/networking/ddns-updater.nix
+++ nixos/modules/services/networking/ddns-updater.nix
@@ -7,6 +7,8 @@
 let
   cfg = config.services.ddns-updater;
 
+  filteredEnvironment = lib.filterAttrs (_: v: v != null) cfg.environment;
+
   settingsFormat = pkgs.formats.json { };
   configFile = settingsFormat.generate "config.json" cfg.settings;
 in
@@ -17,9 +19,22 @@ in
     package = lib.mkPackageOption pkgs "ddns-updater" { };
 
     environment = lib.mkOption {
-      type = lib.types.attrsOf lib.types.str;
-      description = "Environment variables to be set for the ddns-updater service. DATADIR is ignored to enable using systemd DynamicUser. For full list see <https://github.com/qdm12/ddns-updater>";
-      default = { };
+      type = lib.types.submodule {
+        freeformType = lib.types.attrsOf lib.types.str;
+
+        options.CONFIG = lib.mkOption {
+          type = lib.types.nullOr lib.types.str;
+          default = null;
+          description = "Configuration as a single environment variable. Takes precedence over config.json.";
+        };
+
+        options.DATADIR = lib.mkOption {
+          type = lib.types.str;
+          default = "%S/ddns-updater";
+          description = "Data directory for ddns-updater. Defaults to the systemd state directory.";
+        };
+      };
+      description = "Environment variables for ddns-updater, see <https://github.com/qdm12/ddns-updater#environment-variables> for supported values.";
     };
 
     settings = lib.mkOption {
@@ -34,10 +49,11 @@ in
       wantedBy = [ "multi-user.target" ];
       wants = [ "network-online.target" ];
       after = [ "network-online.target" ];
-      environment = cfg.environment // {
-        DATADIR = "%S/ddns-updater";
-      };
-      preStart = lib.mkIf (cfg.settings != { }) ''
+      environment = filteredEnvironment;
+      # CONFIG and settings must not be mutually exclusive: users may set
+      # `CONFIG = builtins.toJSON cfg.settings` to pass structured settings
+      # via environment variable instead of a file.
+      preStart = lib.mkIf (cfg.environment.CONFIG == null && cfg.settings != { }) ''
         ln -sf ${configFile} "$DATADIR/config.json"
       '';
       serviceConfig = {
